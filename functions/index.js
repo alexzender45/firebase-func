9c119e7a04c04b7fab94f695c7c787b929a79b08
alexzender45 <alexzendersamuel33@gmail.com>
2019-10-29 06:58:27 -0700

    added firebase

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-const uuidv5 = require('uuid/v5');
 
-// Firebase init
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Express and CORS middleware init
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+ 
 app.use(cors());
 
-// POST / method
+
 app.post("/", (request, response) => {
-     
     const entry = request.body;
 
-    return admin.database().ref('/user{userId}').push(entry)
+    return admin.database().ref('/messages').push(entry)
         .then(() => {
-            return response.status(200).send(entry, uuidv5('hello.example.com', uuidv5.DNS))
+            return response.status(200).send(entry)
         }).catch(error => {
             console.error(error);
             return response.status(500).send('Oh no! Error: ' + error);
         });
 });
 
-exports.entries = functions.https.onRequest(app);
+exports.addmessage = functions.https.onRequest(app);
+
+
+exports.GenerateUuid = functions.database.instance('todolist-app-275ac').ref('/messages/{UserId}')
+    .onCreate((snapshot, context) => {
+        console.log(snapshot.val());
+        console.log(context);
+        const uuidv5 = require('uuid/v5');
+        const ENYE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+        const code = uuidv5('enye-id', ENYE);
+        return snapshot.ref.update({ userId : code});
+    });
