131daa6c3db1b42651fc8038b05385aadfab7022
alexzender45 <alexzendersamuel33@gmail.com>
2019-10-30 06:01:59 -0700

    updated function

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,22 @@
+'use strict';
 
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Firebase init
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
- 
+// Express and CORS middleware init
+const app = express();
 app.use(cors());
 
-
+// POST / method
 app.post("/", (request, response) => {
     const entry = request.body;
 
-    return admin.database().ref('/messages').push(entry)
+    return admin.database().ref('/entries').push(entry)
         .then(() => {
             return response.status(200).send(entry)
         }).catch(error => {
@@ -35,15 +25,14 @@ app.post("/", (request, response) => {
         });
 });
 
-exports.addmessage = functions.https.onRequest(app);
-
-
-exports.GenerateUuid = functions.database.instance('todolist-app-275ac').ref('/messages/{UserId}')
-    .onCreate((snapshot, context) => {
-        console.log(snapshot.val());
-        console.log(context);
-        const uuidv5 = require('uuid/v5');
-        const ENYE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
-        const code = uuidv5('enye-id', ENYE);
-        return snapshot.ref.update({ userId : code});
+// GET / method
+app.get("/", (request, response) => {
+    return admin.database().ref('/entries').on("value", snapshot => {
+        return response.status(200).send(snapshot.val());
+    }, error => {
+        console.error(error);
+        return response.status(500).send('Oh no! Error: ' + error);
     });
+});
+
+exports.entries = functions.https.onRequest(app);
